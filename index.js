4cfbc989799d9d0638ac47b186b01324e8a8d0ae
Neeoxx <mamannofficiel@gmail.com>
2021-07-01 17:35:22 +0200

    changement de tout

---
+++ index.js
@@ -0,0 +1,38 @@
+const { CommandoClient } = require('discord.js-commando');
+const path = require("path");
+require('dotenv').config();
+
+const client = new CommandoClient({
+    commandPrefix: 'b?',
+    owner: '340145684453785602',
+    invite:'https://discord.gg/R3dGtc6sDw',
+
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerGroup('music' , 'Music')
+    .registerCommandsIn(path.join(__dirname, "commands"));
+
+client.server = {
+    queue: [],
+    currentVideo: {title:"Rien pour le moment.", url:"" },
+    dispatcher: null,
+    connection: null
+    
+};
+
+client.once('ready', () => {
+    console.log(`Connecté en tant que ${client.user.tag} - (${client.user.id})`);
+});
+
+client.on("ready", () => {
+    client.user.setStatus("online");
+    client.user.setActivity("votre conversation", { type: "LISTENING" });
+  });
+
+client.on('error', (error) => console.error(error));
+
+client.login(process.env.TOKEN);
